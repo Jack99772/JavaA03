2b3d7531b5cab5860e6648e9ad861c3ee54cc254
jackosullivan <jack99772@gmail.com>
2023-11-26 16:37:27 +0000

    test for the constructor with a valid palette size parameter

--- src/main/java/cs3318/A03/ColourTable.java
+++ src/main/java/cs3318/A03/ColourTable.java
@@ -1,4 +1,24 @@
 package cs3318.A03;
 
 public class ColourTable {
+    private int paletteSize;
+
+    // Default constructor with no arguments
+    public ColourTable() {
+        // Default behavior, you can initialize default values if needed
+    }
+
+    // Constructor with a palette size parameter
+    public ColourTable(int paletteSize) {
+        this.paletteSize = paletteSize;
+        // Additional initialization logic if needed
+    }
+
+    // Getter method for palette size
+    public int getPaletteSize() {
+        return paletteSize;
+    }
+
+    // Other methods and properties...
+
 }
--- src/test/java/cs3318/A03/ColourTableTest.java
+++ src/test/java/cs3318/A03/ColourTableTest.java
@@ -1,4 +1,53 @@
 package cs3318.A03;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import cs3318.A03.ColourTable;
+class ColourTableTest {
 
-public class ColourTableTest {
+    @Test
+    void constructorValidPaletteSize() {
+        // Arrange
+        int validPaletteSize = 8; // Choose a valid palette size
+
+        // Act
+        ColourTable colourTable = new ColourTable(validPaletteSize);
+
+        // Assert
+        assertNotNull(colourTable, "ColourTable object should not be null");
+        // Add more assertions based on the expected behavior of your constructor
+        assertEquals(validPaletteSize, colourTable.getPaletteSize(), "Palette size should match");
+        // Add more assertions if your constructor initializes other properties
+    }
+
+    @Test
+    void constructorInvalidPaletteSize() {
+        // Write test for constructor with an invalid palette size
+        // Expect an exception to be thrown
+    }
+
+    @Test
+    void addValidColor() {
+        // Write test for adding a valid RGB color to the table
+        // Ensure that the color is added successfully
+    }
+
+    @Test
+    void addInvalidColor() {
+        // Write test for adding an invalid RGB color
+        // Expect an exception to be thrown
+    }
+
+    @Test
+    void addColorAtFullCapacity() {
+        // Write test for adding a color when the table is at full capacity
+        // Expect an exception to be thrown
+    }
+
+    @Test
+    void addSameColorMoreThanOnce() {
+        // Write test for adding the same color more than once
+        // Define the expected behavior
+    }
+
+    // Add more tests as needed for other aspects of the ColourTable class
 }
