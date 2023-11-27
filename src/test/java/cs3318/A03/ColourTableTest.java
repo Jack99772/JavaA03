18395da1aa7d49a24dee4504befcab9c4bac166e
jackosullivan <jack99772@gmail.com>
2023-11-27 18:14:59 +0000

    added docstrings

--- src/test/java/cs3318/A03/ColourTableTest.java
+++ src/test/java/cs3318/A03/ColourTableTest.java
@@ -2,8 +2,18 @@ package cs3318.A03;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import cs3318.A03.ColourTable;
+/**
+ * The ColourTableTest class contains a set of JUnit tests for the ColourTable class.
+ * Each test method corresponds to a specific functionality of the ColourTable class,
+ * validating its behavior under various scenarios. These tests ensure that the ColourTable
+ * class adheres to the specified requirements and behaves as expected in different scenarios.
+ */
 class ColourTableTest {
 
+    /**
+     * The constructorValidPaletteSize function tests the constructor of the ColourTable class.
+     * It checks that a valid palette size is accepted and that it initializes other properties correctly.
+     */
     @Test
     void constructorValidPaletteSize() {
         // Arrange
@@ -19,6 +29,10 @@ class ColourTableTest {
         // Add more assertions if your constructor initializes other properties
     }
 
+    /**
+     * The constructorInvalidPaletteSize function tests the ColourTable constructor with an invalid palette size.
+     * The test should fail if the constructor does not throw an IllegalArgumentException when given a palette size of 1.
+     */
     @Test
     void constructorInvalidPaletteSize() {
         // Arrange
@@ -33,6 +47,11 @@ class ColourTableTest {
         });
     }
 
+    /**
+     * The addValidColor function tests the add function of the ColourTable class.
+     * It creates a ColourTable with a valid palette size, and adds a valid RGB color to it.
+     * The test passes if the colour is added successfully.
+     */
     @Test
     void addValidColor() {
         // Arrange
@@ -49,6 +68,10 @@ class ColourTableTest {
         // You might add more assertions based on your specific requirements
     }
 
+    /**
+     * The addInvalidColor function tests the add function of the ColourTable class.
+     * It checks that an IllegalArgumentException is thrown when attempting to add a color outside of the valid range.
+     */
     @Test
     void addInvalidColor() {
         // Arrange
@@ -64,6 +87,11 @@ class ColourTableTest {
         });
     }
 
+    /**
+     * The addColorAtFullCapacity function tests the add function of the ColourTable class.
+     * It creates a new ColourTable object with a small palette size for testing purposes, and adds two colors to it.
+     * Then, it attempts to add a third color when the table is at full capacity and expects an IllegalStateException to be thrown.
+     */
     @Test
     void addColorAtFullCapacity() {
         // Arrange
@@ -82,6 +110,10 @@ class ColourTableTest {
         }, "Expected IllegalStateException to be thrown, but nothing was thrown.");
     }
 
+    /**
+     * The addSameColorMoreThanOnce function tests the add function to ensure that it throws an IllegalArgumentException
+     * when attempting to add the same color more than once.
+     */
     @Test
     void addSameColorMoreThanOnce() {
         // Arrange
@@ -97,6 +129,4 @@ class ColourTableTest {
             colourTable.add(redColor);
         });
     }
-
-    // Add more tests as needed for other aspects of the ColourTable class
 }
